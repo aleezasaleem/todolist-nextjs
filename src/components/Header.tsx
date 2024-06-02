a3961e41bca0067436f67740427ff416e34fef9d
gh-tayyab <muhammadtayyabmt0@gmail.com>
2024-06-02 01:02:32 -0700

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import { FaGithub } from "react-icons/fa"
 
 const Header = () => {
   return (
-    <div className="w-full bg-stone-800 flex justify-between">
-        <div className="text-white text-xl p-2">Todo List</div>
+    <div className="w-full bg-stone-800  h-[12vh] flex items-center justify-between px-4">
+        <div className="text-white text-xl md:text-2xl p-2 font-semibold">Todo List</div>
         <div className="text-white text-xl p-2">
-            <Link href={"https://github.com/gh-tayyab"} target="_blank" rel="noopener norefferrer">
-                <FaGithub size={30}/>
+            <Link href={"https://github.com/aleezasaleem"} target="_blank" rel="noopener norefferrer">
+                <FaGithub size={40}/>
             </Link>
         </div>
     </div>
